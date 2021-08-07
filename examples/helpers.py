412da83c3bccdda8c29032cd571207d446bb6eba
hp0404 <hryhorii.pavlenko@gmail.com>
2021-08-07 23:50:09 +0300

    Add helper utilities

---
+++ examples/helpers.py
@@ -0,0 +1,20 @@
+import json
+import pathlib
+from typing import Any, Dict, Iterator
+
+# path
+ROOT = pathlib.Path("./").resolve().parent
+RAW = ROOT / "data" / "raw"
+
+meta_path = RAW / "metadata.jsonl"
+texts_path = RAW / "texts.jsonl"
+
+# typing classes
+Publication = Dict[str, Any]
+
+
+def read_jsonl(p: pathlib.PosixPath) -> Iterator[Publication]:
+    """Yield .jsonl file's contents line by line."""
+    with open(p) as lines:
+        for line in lines:
+            yield json.loads(line)
